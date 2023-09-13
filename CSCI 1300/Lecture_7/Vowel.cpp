e3f368552a7fa6efe66f18fdb538f5b5545ed8a2
August Penny <august@engr2-4-94-dhcp.int.colorado.edu>
2023-09-13 15:34:52 -0600

    9/12/2023

---
+++ CSCI 1300/Lecture_7/Vowel.cpp
@@ -0,0 +1,28 @@
+ #include<iostream>
+#include<iomanip>
+
+
+using namespace std;
+
+
+
+
+double areaOfCircle(double radius)
+{
+    //takes in a radius and prints out an area
+}
+
+int main()
+{
+    const double pi = 3.14;
+    double radius = 0;
+
+    cout << "enter radius: " << endl;
+    cin >> radius;
+
+    double area = pi * radisu * radius;
+
+    cout << "area: " << area << endl;
+
+    return 0;
+}
